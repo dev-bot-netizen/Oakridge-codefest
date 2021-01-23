7725cf4b96188a4f0312c6888bc3c6f22e345a36
dev-bot-netizen <goelakshaj@gmail.com>
2021-01-23 10:41:58 +0530

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,28 @@
+from tkinter import * 
+
+root = Tk()
+
+fname = input('What is your first name? ')
+lname = input('What is your first name? ')
+age = input('What is your first name? ')
+pincode = input('What is your first name? ')
+
+print('difficulty breathing or shortness of breath')
+print('chest pain or pressure')
+print('loss of speech or movement')
+print('aches and pains')
+print('sore throat')
+print('diarrhoea')
+print('conjunctivitis')
+print('headache')
+print('loss of taste or smell')
+print('A rash on skin, or discolouration of fingers or toes')
+print('fever')
+print('dry cough')
+print('tiredness')
+nxt = input('Do you experience any of the above?(1 for yes, 0 for no)')
+
+temp = input('What is your temperature(Use a thermometer)? ')
+Bp = input('What is your blood pressure(A sphygmomanometer can be used)? ')
+O2 = input('what is your oxygen level(An electronic oxygen measuring device can be used)? ')
+Eg = input('Does you ECG test give you any problems(ECG = electrocardiogram test 0 for yes 1 for no)? ')
